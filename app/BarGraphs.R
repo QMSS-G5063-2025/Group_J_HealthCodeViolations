62ff97e30ea0e16033a5f7e81f3331ef409d7754
Katherine Lin <152942105+katherinelin2@users.noreply.github.com>
2025-04-27 16:26:07 -0400

    Update bar graphs helper filer

--- app/BarGraphs.R
+++ app/BarGraphs.R
@@ -1,25 +1,58 @@
 # WHICH BOROUGHS HAD THE MOST VIOLATIONS?
 
 borough_violations_func <- function(data) {
-  data %>%
+  temp <- data %>%
     filter(critical.flag != 'Not Applicable') %>% 
     count(boro) %>% 
     arrange(desc(n))
+  return(temp)
 }
 
 # plotting from last function 
-borough_violations_plot_func <- function(boroughviolations) {
-  ggplot(boroughviolations, aes(x = reorder(boro, n), y = n, fill = boro)) +
-    geom_col() +
+borough_violations_plot_func <- function(data) {
+  boroughviolations <- borough_violations_func(data)
+  
+  colors_borough <- c("Manhattan" = "#6D9AC6",  
+                      "Brooklyn" = "#F0A88C",  
+                      "Queens" = "#A1D6B9", 
+                      "Bronx" = "#F296B3",   
+                      "Staten Island" = "#C89BCC")
+  
+  graphtemp <- ggplot(boroughviolations, aes(x = reorder(boro, n), y = n, fill = boro)) +
+    geom_col(aes(text = paste(
+      "Violation Count: ", n
+    ))) +
+    scale_fill_manual(values = colors_borough) +
     coord_flip() +
-    ggtitle("Which boroughs had the most violations?") +
-    xlab("Borough") + ylab("Number of violations") + labs(color = "Borough") + 
-    theme_minimal()
+    xlab("Borough") + ylab("Number of violations") +
+    guides(fill = guide_legend(title = "Borough")) +
+    theme_minimal(base_size = 14) +
+    theme(
+          plot.background = element_rect(fill = "#f8f9fa"),
+          axis.title = element_text(size = 11, face = "plain"),
+          axis.text = element_text(size = 10, color = "#555555"),
+          legend.title = element_text(size = 11, face = "plain"),
+          legend.text = element_text(size = 10),
+          panel.grid.major = element_line(color = "#DDDDDD", size = 0.5), 
+          panel.grid.minor = element_line(color = "#DDDDDD", size = 0.25),
+          axis.text.x = element_text(),
+          axis.text.y = element_text())
+  
+  graph <- ggplotly(graphtemp, tooltip = "text") %>%
+    layout(
+      xaxis = list(
+        showticklabels = TRUE
+      ),
+      yaxis = list(
+        fixedrange = FALSE
+      ),
+      hoverlabel = list(
+        bgcolor = "white",
+        font = list(color = "black")
+      )
+    )
+  return(graph)
 }
-
-
-
-
 
 
 # MOST COMMON VIOLATIONS BY BOROUGH (TOP 10)
@@ -29,18 +62,28 @@ borough_common_violations_func <- function(data) {
   violationmerge <- data %>% # need to clean up violation.descriptions since some entries have extra whitespace, etc.
     select(violation.code, violation.description) %>%
     distinct(violation.code, .keep_all = TRUE)
-  data %>%
+  
+  temp <- data %>%
     filter(critical.flag != 'Not Applicable') %>%
     count(boro, violation.code, name = 'violation.code.count') %>%
     left_join(violationmerge, by = "violation.code") %>% # merging with unique violation description data
     group_by(boro) %>%
     arrange(desc(violation.code.count)) %>%  # ordering by descending order
     slice_head(n = 10) # getting top 10 per borough
+  
+  return(temp)
 }
 
 
 # plotting from last function 
-borough_common_violations_plot_func <- function(boroughcommonviolationsdata) {
+borough_common_violations_plot_func <- function(data) {
+  boroughcommonviolationsdata <- borough_common_violations_func(data)
+  colors_borough <- c("Manhattan" = "#6D9AC6",  
+                      "Brooklyn" = "#F0A88C",  
+                      "Queens" = "#A1D6B9", 
+                      "Bronx" = "#F296B3",   
+                      "Staten Island" = "#C89BCC")
+  
   plot <- ggplot(boroughcommonviolationsdata, aes(
     x = reorder(violation.code, violation.code.count), 
     y = violation.code.count, 
@@ -52,16 +95,40 @@ borough_common_violations_plot_func <- function(boroughcommonviolationsdata) {
       "<br>Number of Violations:", violation.code.count
     )
   )) +
+    scale_fill_manual(values = colors_borough) +
     geom_col() +
-    facet_grid(. ~ boro) + 
-    ggtitle("What are the most common violations per borough?") +
+    facet_grid(. ~ boro) +
     xlab("Violation Type") + 
     ylab("Number of Violations") + 
-    labs(color = "Borough") +
-    theme_minimal()
+    guides(fill = guide_legend(title = "Borough")) +
+    theme_minimal(base_size = 14) +
+    theme(
+      plot.background = element_rect(fill = "#f8f9fa"),
+      axis.title = element_text(size = 11, face = "plain"),
+      axis.text = element_text(size = 10, color = "#555555"),
+      legend.title = element_text(size = 11, face = "plain"),
+      legend.text = element_text(size = 10),
+      panel.grid.major = element_line(color = "#DDDDDD", size = 0.5), 
+      panel.grid.minor = element_line(color = "#DDDDDD", size = 0.25),
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      axis.text.y = element_text())
   
-  ggplotly(plot, tooltip = "text")
+  plot2 <- ggplotly(plot, tooltip = "text")%>%
+    layout(
+      xaxis = list(
+        showticklabels = TRUE
+      ),
+      yaxis = list(
+        fixedrange = FALSE
+      ),
+      hoverlabel = list(
+        bgcolor = "white",
+        font = list(color = "black")
+      )
+    )
+  return(plot2)
 }
 
+# MOST COMMON CRITICAL VIOLATIONS
 
 
