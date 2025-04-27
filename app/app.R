a60752b15e5b446056bc9aafe8e53c75e992be1f
Katherine Lin <152942105+katherinelin2@users.noreply.github.com>
2025-04-27 16:26:38 -0400

    Update app.R

--- app/app.R
+++ app/app.R
@@ -8,12 +8,13 @@ library(ggplot2)
 library(leaflet)
 library(leaflet)
 library(htmltools)
+library(plotly)
+library(lubridate) 
 
+# Helper Function Files
 source("~/Desktop/Data_Viz/website/map_functions.R")
-
-# Load income data
-
-# Load reviews
+source("~/Desktop/Data_Viz/website/line_functions.R")
+source("~/Desktop/Data_Viz/website/BarGraphs.R")
 
 ui <- navbarPage(
   title = "NYC Health Code Violations",
@@ -99,20 +100,26 @@ ui <- navbarPage(
                     padding-bottom: 60px;
                     font-size: 18px;
                   }
+                  .single-figure{
+                    margin-left: 30px;
+                    padding-right: 200px;
+                    padding-left: 200px;
+                    padding-bottom: 10px;
+                  }
                 "))
   ),
   tabPanel("Home",
            fluidPage(
                  tags$div(class = "custom-header",
                           h1("Health Code Violations in NYC"),
-                          h3("An exploration of trends in restaurant health code violations across New York City"),
+                          h3("An exploration of trends in restaurant health code violations across New York City between 2015 - 2023"),
                           h4("Katherine Lin, Humaira Ahmed, and Juna Kawai-Yue")
                  ),
                  tags$div(class = "custom-container",
                           h3("What's up with NYC restaurants?"),
                           tags$div(class = "custom-text",
                                   p("Our team was interested in investigating patterns in restaurant health code violations across New York City. We wanted to look at a few key aspects of these violations in tandem with reviews of these restaurants to see whether or not health code violations impact customer experiences but also what general trends there are within the city regarding these violations."),
-                                  p("Within this website, you will find maps, barplots, and line plots that demonstrate trends in the number of violations across NYC and between boroughs, cuisines, types of violations, and the customer experience and how these factors relate to socioeconomic makeups of the neighborhoods."))),
+                                  p("Within this website, you will find maps, barplots, word clouds, and line plots that demonstrate trends in the number of violations across NYC and between boroughs, cuisines, types of violations, and the customer experience and how these factors relate to socioeconomic makeups of the neighborhoods."))),
                  tags$div(class = "custom-container",
                           h3("This website explores health code violations in NYC in the following ways:"),
                           tags$div(class = "custom-row",
@@ -123,8 +130,8 @@ ui <- navbarPage(
                                    ),
                                    tags$div(class = "col-md-4 feature",
                                             tags$i(class = "fas fa-triangle-exclamation"),
-                                            h3("Critical Violations"),
-                                            h4("Look closer at the most critical and common health code violations.")
+                                            h3("Violations"),
+                                            h4("Look closer at trends around specific health code violations.")
                                    ),
                                    tags$div(class = "col-md-4 feature",
                                             tags$i(class = "fas fa-quote-right"),
@@ -153,9 +160,21 @@ ui <- navbarPage(
              tags$div(class = "custom-container",
                       h2("Trends in Health Code Violations Across NYC and by Borough"),
                       tags$div(class = "custom-text",
-                               p("The section breaks down the overall trends of health code violations across New York City and within each borough and how other factors, like socioeconomic status are related."))
+                               p("This page breaks down the greater trends of health code violations across New York City and within each borough and how other factors, like socioeconomic status are related."))
+             ),
+             tags$div(class = 'custom-container',
+                      h3("Trends from 2019-2023"),
+                      tags$div(class = "custom-row",
+                        plotlyOutput("last_5"),
+                        plotlyOutput("last_5_month"))
+             ),
+             tags$div(class = 'custom-container',
+                      h3("Number of Violations Across Boroughs"),
+                      tags$div(class = 'single-figure',
+                              plotlyOutput("by_borough")),
              ),
              tags$div(class = "custom-dropdown",
+                      h3("Borough Violations"),
                       selectInput("region", "Region to Visualize:", 
                                   c(
                                     "All of NYC" = "nyc",
@@ -169,17 +188,22 @@ ui <- navbarPage(
                       leafletOutput("region_map", height = 600)
              )
            )),
-  tabPanel('Critical Violations',
+  tabPanel('Violations',
            fluidPage(
               tags$div(class = "custom-container",
-                       h2("Trends in Critical Health Code Violations"),
+                       h2("Trends in Health Code Violations"),
                        tags$div(class = "custom-text",
-                                p("This page dives deeper into a range of different types of violations, more specifically, the top 10 most common critical violations across New York City."))
+                                p("This page dives deeper into a range of different types of violations, including, the top 10 most common violations across New York City."))
                  ),
+              tags$div(class = "custom-container",
+                       h3("Top 10 Most Common Violations by Borough"),
+                       tags$div(class = "custom-text",
+                                p("The following visualizations highlights these violations through their violation codes. Hover over each bar to read what each represents.")),
+                       plotlyOutput("viol_borough")),
               tags$div(class = 'custom-container',
                        h3("Maps of Top 10 Most Common Critical Violations Across NYC"),
                        tags$div(class = "custom-text",
-                                p("The following maps allow you to explore restaurants who have been issued violations of the following health code violations:")),
+                                p("The following maps allow you to explore restaurants who have been issued violations of the following critical health code violations:")),
                        tags$div(class = 'custom-list2',
                                 tags$ol(
                                   tags$li('Food contact surface not properly washed, rinsed and sanitized after each use and following any activity when contamination may have occurred'),
@@ -213,13 +237,33 @@ ui <- navbarPage(
            )),
   tabPanel('Google Reviews',
            fluidPage(
-             h2("Text Analysis on Google Reviews from Top Violation Restaurants per Borough")
+             tags$div(class = "custom-container",
+                      h2("Text Analysis on Google Reviews from Top Violation Restaurants per Borough"),
+                      tags$div(class = "custom-text",
+                               p("This page covers text analysis of the 5 most recent reviews from the top 10 restaurants per borough with the most health code violations."))
+             )
            ))
 )
 
 server <- function(input, output) {
   # Load health code violations data
   hcv <- read.csv("~/Desktop/Data_Viz/hcvyear.csv")
+  nyczips <- read.csv("~/Desktop/Data_Viz/nyc-zip-codes.csv")
+  dta <- read.csv("~/Desktop/Data_Viz/22zpallagi.csv")
+  
+  # filter dta to only rows with zip code in the list of NYC Zip Codes
+  dta_nyc <- dta[dta$zipcode %in% nyczips$ZipCode, ]
+  
+  income_filtered <- dta_nyc %>%
+    select(STATE, zipcode, agi_stub, A00100, A02650)
+  
+  # average AGI and income per ZIP code
+  avg_income <- dta_nyc %>%
+    group_by(zipcode) %>%
+    summarise(
+      avg_AGI = mean(A00100, na.rm = TRUE),
+      avg_income = mean(A02650, na.rm = TRUE)
+    )
   
   # handle regional maps
   output$region_map <- renderLeaflet({
@@ -229,6 +273,24 @@ server <- function(input, output) {
   # handle violation maps
   output$viol_map <- renderLeaflet({
     map_critviolations(hcv, input$violation)
+  })
+  
+  # handle line plots
+  output$last_5 <- renderPlotly({
+    plot_violations_by_borough(hcv)
+  })
+  
+  output$last_5_month <- renderPlotly({
+    plot_violations_by_borough2(hcv)
+  })
+  
+  # handle bar graphs
+  output$by_borough <- renderPlotly({
+    borough_violations_plot_func(hcv)
+  })
+  
+  output$viol_borough <- renderPlotly({
+    borough_common_violations_plot_func(hcv)
   })
 }
 shinyApp(ui = ui, server = server)
